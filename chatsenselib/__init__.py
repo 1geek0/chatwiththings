cbb249e224a1ef906e4ad11d014b7f9e980711e2
Jari Arkko <jari.arkko@piuha.net>
2017-10-01 13:01:45 +0200

    forgotten

--- chatsenselib/__init__.py
+++ chatsenselib/__init__.py
@@ -1,3 +1,4 @@
-__all__ = ["sensors", "variables"]
+__all__ = ["sensors", "variables","parser1"]
 from chatsenselib.sensors import *
 from chatsenselib.variables import *
+from chatsenselib.parser1 import *
--- spacy-test.py
+++ spacy-test.py
@@ -1,3 +1,12 @@
 import spacy
+from chatsenselib.parser1 import *
+
 nlp = spacy.load('en')
-doc = nlp(u'This is a sentence.')
+doc1 = nlp(u'This is a sentence.')
+doc2 = nlp(u'What is the temperature in the living room?')
+s2 = doc2.print_tree()[0]
+
+print("Looking for requests in " + doc2.text);
+        
+for s in doc2.print_tree():
+    lookforrequest(s)
