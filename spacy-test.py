771bbd114c46d4e5baf433cb07b25d052f6f1b5a
NilayKulkarni <typhoscientic@gmail.com>
2017-10-01 14:42:21 +0200

    Removed the str() function for the input

--- spacy-test.py
+++ spacy-test.py
@@ -5,7 +5,7 @@ from chatsenselib.variables import *
 my_question = input("How can I help you?\n")
 nlp = spacy.load('en')
 doc1 = nlp(u'This is a sentence.')
-doc2 = nlp(str(my_question))
+doc2 = nlp(my_question)
 s2 = doc2.print_tree()[0]
 
 print("Looking for requests in " + doc2.text);
